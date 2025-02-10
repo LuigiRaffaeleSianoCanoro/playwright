61bc6ec46b57d0cbf1504c8fb2c2a2aaa907650f
LuigiCanoro <phoenix_luigi@hotmail.com>
2025-02-10 15:43:33 -0300

    added 3 login tests

--- tests/userAuth.spec.ts
+++ tests/userAuth.spec.ts
@@ -1,6 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('', async ({ page }) => {
+test('Login with valid credentials', async ({ page }) => {
+    await page.goto('https://demowebshop.tricentis.com/')
+    await page.getByRole('link', {name: 'log in'}).click()
+    await page.getByRole('textbox', {name: 'Email'}).type('towime9934@intady.com')
+    await page.getByRole('textbox', {name: 'Password'}).type('1q2w3e4r')
+    await page.getByRole('button', {name: 'Log in'}).click()
+    await expect(page.locator('div[class="header-links"] a[href="/customer/info"]')).toHaveText('towime9934@intady.com')
+});
 
+test('Login with invalid credentials', async ({ page }) => {
+    await page.goto('https://demowebshop.tricentis.com/')
+    await page.getByRole('link', {name: 'log in'}).click()
+    await page.getByRole('textbox', {name: 'Email'}).type('towime9934@intady.com')
+    await page.getByRole('textbox', {name: 'Password'}).type('wrongPassword')
+    await page.getByRole('button', {name: 'Log in'}).click()
+    await expect(page.locator('div.message-error span')).toHaveText('Login was unsuccessful. Please correct the errors and try again.')
 });
 
